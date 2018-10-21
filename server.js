05f532503f70718cdbe792067e30b48f90159921
diamondrubix <Diamondrubix@gmail.com>
2018-10-21 03:39:15 -0500

    test

--- server.js
+++ server.js
@@ -75,10 +75,15 @@ io.on('connection', function(socket){
 });
 
 
+var port = 80;
 
+http.on('error', function(e){
+    port = 3001
+    http.listen(port, function () {
+        console.log('listening on *:3001');
+    });
 
-
-
-http.listen(3001, function(){
-    console.log('listening on *:3001');
+})
+http.listen(port, function () {
+    console.log('listening on 80');
 });
